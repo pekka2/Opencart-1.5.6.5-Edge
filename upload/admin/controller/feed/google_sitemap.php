462e0bf7fc5edd83d2a4f03901a4429daacbd8c0
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-06-28 20:00:37 +0300

    Add files via upload

--- upload/admin/controller/feed/google_sitemap.php
+++ upload/admin/controller/feed/google_sitemap.php
@@ -1,16 +1,16 @@
 <?php 
-class ControllerFeedGoogleBase extends Controller {
+class ControllerFeedGoogleSitemap extends Controller {
 	private $error = array(); 
 
 	public function index() {
-		$this->language->load('feed/google_base');
+		$this->language->load('feed/google_sitemap');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('setting/setting');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('google_base', $this->request->post);				
+			$this->model_setting_setting->editSetting('google_sitemap', $this->request->post);				
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
@@ -46,29 +46,29 @@ class ControllerFeedGoogleBase extends Controller {
 
 		$this->data['breadcrumbs'][] = array(
 			'text'      => $this->language->get('text_feed'),
-			'href'      => $this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL'),
+			'href'      => $this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL'),       		
 			'separator' => ' :: '
 		);
 
 		$this->data['breadcrumbs'][] = array(
 			'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('feed/google_base', 'token=' . $this->session->data['token'], 'SSL'),
+			'href'      => $this->url->link('feed/google_sitemap', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
 		);
 
-		$this->data['action'] = $this->url->link('feed/google_base', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['action'] = $this->url->link('feed/google_sitemap', 'token=' . $this->session->data['token'], 'SSL');
 
 		$this->data['cancel'] = $this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL');
 
-		if (isset($this->request->post['google_base_status'])) {
-			$this->data['google_base_status'] = $this->request->post['google_base_status'];
+		if (isset($this->request->post['google_sitemap_status'])) {
+			$this->data['google_sitemap_status'] = $this->request->post['google_sitemap_status'];
 		} else {
-			$this->data['google_base_status'] = $this->config->get('google_base_status');
+			$this->data['google_sitemap_status'] = $this->config->get('google_sitemap_status');
 		}
 
-		$this->data['data_feed'] = HTTP_CATALOG . 'index.php?route=feed/google_base';
+		$this->data['data_feed'] = HTTP_CATALOG . 'index.php?route=feed/google_sitemap';
 
-		$this->template = 'feed/google_base.tpl';
+		$this->template = 'feed/google_sitemap.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
@@ -78,7 +78,7 @@ class ControllerFeedGoogleBase extends Controller {
 	} 
 
 	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'feed/google_base')) {
+		if (!$this->user->hasPermission('modify', 'feed/google_sitemap')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 
