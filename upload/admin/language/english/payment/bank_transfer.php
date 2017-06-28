54e12561bfca355197aaa99847efe9cc2ae69912
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-06-28 22:37:12 +0300

    Add files via upload

--- upload/admin/language/english/payment/bank_transfer.php
+++ upload/admin/language/english/payment/bank_transfer.php
@@ -1,12 +1,13 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cash On Delivery';
+$_['heading_title']      = 'Bank Transfer';
 
-// Text
+// Text 
 $_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified Cash On Delivery payment module!';
+$_['text_success']       = 'Success: You have modified bank transfer details!';
 
 // Entry
+$_['entry_bank']         = 'Bank Transfer Instructions:';
 $_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
 $_['entry_order_status'] = 'Order Status:';
 $_['entry_geo_zone']     = 'Geo Zone:';
@@ -14,5 +15,6 @@ $_['entry_status']       = 'Status:';
 $_['entry_sort_order']   = 'Sort Order:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Cash On Delivery!';
+$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
+$_['error_bank']         = 'Bank Transfer Instructions Required!';
 ?>
--- upload/admin/language/english/payment/cheque.php
+++ upload/admin/language/english/payment/cheque.php
@@ -1,12 +1,13 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cash On Delivery';
+$_['heading_title']      = 'Cheque / Money Order';
 
-// Text
+// Text 
 $_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified Cash On Delivery payment module!';
+$_['text_success']       = 'Success: You have modified cheque / money order account details!';
 
 // Entry
+$_['entry_payable']      = 'Payable To:';
 $_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
 $_['entry_order_status'] = 'Order Status:';
 $_['entry_geo_zone']     = 'Geo Zone:';
@@ -14,5 +15,6 @@ $_['entry_status']       = 'Status:';
 $_['entry_sort_order']   = 'Sort Order:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Cash On Delivery!';
+$_['error_permission']   = 'Warning: You do not have permission to modify payment cheque / money order!';
+$_['error_payable']      = 'Payable To Required!';
 ?>
