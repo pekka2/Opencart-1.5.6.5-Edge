8f382da5a5baa4c868f2828c8d4e1f830e9ccc82
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-06-28 22:27:35 +0300

    Add files via upload

--- upload/admin/language/english/module/affiliate.php
+++ upload/admin/language/english/module/affiliate.php
@@ -1,10 +1,10 @@
 <?php
 // Heading
-$_['heading_title']       = 'Account';
+$_['heading_title']       = 'Affiliate';
 
 // Text
 $_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module account!';
+$_['text_success']        = 'Success: You have modified module affiliate!';
 $_['text_content_top']    = 'Content Top';
 $_['text_content_bottom'] = 'Content Bottom';
 $_['text_column_left']    = 'Column Left';
@@ -17,5 +17,5 @@ $_['entry_status']        = 'Status:';
 $_['entry_sort_order']    = 'Sort Order:';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module account!';
+$_['error_permission']    = 'Warning: You do not have permission to modify module affiliate!';
 ?>
---
+++ upload/admin/language/english/module/change_module_css.php
@@ -0,0 +1,21 @@
+<?php
+$_['heading_title']    = 'Change Module CSS';
+
+$_['text_edit']  = 'Edit Change Module CSS extension';
+$_['text_bestseller']  = 'Bestseller module';
+$_['text_featured']    = 'Featured module';
+$_['text_latest']         = 'Latest module';
+$_['text_module']      = 'Modules';
+$_['text_special']      = 'Special module';
+$_['text_success']     = 'Success: You have modified Change Module CSS extension!';
+
+$_['entry_lg']    = 'Css lg width';
+$_['entry_md']    = 'Css md width';
+$_['entry_caption_height']    = 'Caption height';
+$_['entry_status']     = 'Status';
+$_['entry_tax_status']     = 'Tax Status';
+$_['entry_description_status']     = 'Description Status';
+$_['entry_product_name']     = 'Product name length';
+$_['entry_strlen']     = 'Description text length';
+
+$_['error_permission'] = 'Warning: You do not have permission to modify Change Module CSS extension!';
