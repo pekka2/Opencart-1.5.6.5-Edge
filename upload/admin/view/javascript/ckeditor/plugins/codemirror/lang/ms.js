6f01cc4d72f47df11c3c47ceb1e90ed337a39480
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-06-30 11:00:33 +0300

    Add files via upload

--- upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/ms.js
+++ upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/ms.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'codemirror', 'af', {
-	toolbar: 'Bron'
+CKEDITOR.plugins.setLang( 'codemirror', 'ms', {
+	toolbar: 'Sumber'
 });
