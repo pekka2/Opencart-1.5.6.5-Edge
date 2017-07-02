e50ccced17d1a3f87eff8a35083e7faadb91dfc4
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-07-02 13:53:13 +0300

    Add files via upload

--- upload/catalog/language/english/common/header.php
+++ upload/catalog/language/english/common/header.php
@@ -1,1 +1,18 @@
+<?php
+// Text
+$_['text_home']           = 'Home';
+$_['text_wishlist']       = 'Wish List (%s)';
+$_['text_shopping_cart']  = 'Shopping Cart';
+$_['text_search']         = 'Search';
+$_['text_welcome']        = 'Welcome visitor you can <a href="%s">login</a> or <a href="%s">create an account</a>.';
+$_['text_logged']         = 'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
+$_['text_account']        = 'My Account';
+$_['text_checkout']       = 'Checkout';
+$_['text_category']      = 'Categories';
+$_['text_register']      = 'Register';
+$_['text_login']         = 'Login';
+$_['text_all']           = 'Show All';
+$_['text_discount']      = ' or more ';
+$_['text_logout']        = 'Logout';
 
+?>
