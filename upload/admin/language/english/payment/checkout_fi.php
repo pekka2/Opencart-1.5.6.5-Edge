061de22f88d8705e5cedde5bebb65fa425c08025
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-07-22 14:15:21 +0300

    Add files via upload

---
+++ upload/admin/language/english/payment/checkout_fi.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'Checkout FI';
+
+// Text 
+$_['text_payment']       = 'Maksutavat';
+$_['text_success']       = 'Maksutavan Checkout muokkaaminen suoritettu!';
+$_['text_edit']          = 'Muokkaa Checkout maksutapaa';
+      
+// Entry
+$_['entry_merchant']     = 'Myyj&auml;n tunniste (MERCHANT):';
+$_['entry_security']     = 'Turva-avain:';
+$_['entry_callback']     = 'Paluu-linkki:<br /><span class="help">Paluu osoite kun ostos on suoritettu hyv&auml;ksytysti.</span>';
+$_['entry_order_status'] = 'Tilauksen tila:';
+$_['entry_geo_zone']     = 'Maantiet. alue:';
+$_['entry_status']       = 'Tila:';
+$_['entry_sort_order']   = 'J&auml;rjestys:';
+
+// Error
+$_['error_permission']   = 'Varoitus: Sinulla ei ole oikeuksia maksutavan Checkout muokkaamiseen!';
+$_['error_merchant']     = 'Myyj&auml;n tunniste (MERCHANT) on pakollinen tieto!';
+$_['error_security']     = 'Turva-avain on pakollinen tieto!';
+?>
