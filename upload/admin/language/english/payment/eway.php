e4385824163ef0767c363f65df59d2e6692b74f2
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-07-24 17:14:15 +0300

    Add files via upload

---
+++ upload/admin/language/english/payment/eway.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']      = 'eWAY Payment';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified your eWAY module details!';
+$_['text_eway']   = '<a onclick="window.open(\'http://www.eway.com.au/\');"><img src="view/image/payment/eway_au.jpg" alt="eWAY AU" title="eWAY AU" style="border: 1px solid #cccccc;" /></a>';
+
+// Entry
+$_['entry_test']         = 'Test mode:';
+$_['entry_order_status'] = 'Order status:';
+
+$_['entry_status']       = 'Status:';
+$_['entry_username']	 = 'eWAY API Key:';
+$_['entry_password']	 = 'eWAY password:';
+$_['entry_status']       = 'Status:';
+$_['entry_payment_type'] = 'Payment Type:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_sort_order']   = 'Sort order:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify the eWAY payment module';
+$_['error_username'] = 'eWAY API Key is required!';
+$_['error_password'] = 'eWAY password is required!';
+
+//Help hints
+$_['help_testmode']   = '<span style="font-size:11px; color:#999">You can set to go to testing mode here.</span>';
+$_['help_username']   = '<span style="font-size:11px; color:#999">Your eWAY API key from eWAY website.</span>';
+$_['help_password']   = '<span style="font-size:11px; color:#999">Your eWAY API password from eWAY website.</span>';
+$_['help_payment_type']   = '<span style="font-size:11px; color:#999">The types of payment you accept.</span>';
+$_['help_ewaystatus']   = '<span style="font-size:11px; color:#999">Do you want to authorise payment through eWAY Payment?</span>';
+$_['help_setorderstatus']   = '<span style="font-size:11px; color:#999">Set the status of orders made with this payment module to this value.</span>';
+$_['help_sort_order']   = '<span style="font-size:11px; color:#999">Set the display order in relation to other payment options. The higher the number the "heavier" it is.</span>';
+
+?>
