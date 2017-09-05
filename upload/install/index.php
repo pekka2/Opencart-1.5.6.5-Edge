d0b28b7e51fdf2ac022d0bdd7a64bdc7654f71b7
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-09-05 11:49:02 +0300

    Update index.php

--- upload/install/index.php
+++ upload/install/index.php
@@ -11,6 +11,8 @@ define('DIR_APPLICATION', str_replace('\'', '/', realpath(dirname(__FILE__))) . 
 define('DIR_SYSTEM', str_replace('\'', '/', realpath(dirname(__FILE__) . '/../')) . '/system/');
 define('DIR_OPENCART', str_replace('\'', '/', realpath(DIR_APPLICATION . '../')) . '/');
 define('DIR_DATABASE', DIR_SYSTEM . 'database/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'modification/');
+define('DIR_PAYMENT', DIR_SYSTEM . 'library/payment/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
