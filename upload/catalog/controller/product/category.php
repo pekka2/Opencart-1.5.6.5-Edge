9be9fee2cfa7e723a524c2a9e30a638c4b5d1014
Ernie <webmaster@ip-cam.li>
2018-09-09 20:50:52 +0200

    Update category.php

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -387,10 +387,8 @@ class ControllerProductCategory extends Controller {
 			$pagination->limit = $limit;
 			$pagination->text = $this->language->get('text_pagination');
 			$pagination->url = $this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&page={page}');
-
 			$this->data['pagination'] = $pagination->render();
 			$this->data['results'] = sprintf($this->language->get('text_pagination'), ($product_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($product_total - $limit)) ? $product_total : ((($page - 1) * $limit) + $limit), $product_total, ceil($product_total / $limit));
-
 
 			$this->data['sort'] = $sort;
 			$this->data['order'] = $order;
