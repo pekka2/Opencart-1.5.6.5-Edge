bf443e0fc03456b3f7215242a02aefbce710ac9a
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-17 18:26:07 +0300

    Update slideshow.php

--- upload/catalog/controller/module/slideshow.php
+++ upload/catalog/controller/module/slideshow.php
@@ -6,7 +6,6 @@ class ControllerModuleSlideshow extends Controller {
 		$this->load->model('design/banner');
 		$this->load->model('tool/image');
 		
-		$this->document->addScript('catalog/view/javascript/jquery/nivo-slider/jquery.nivo.slider.pack.js');
 		$this->document->addStyle('catalog/view/javascript/jquery/owl-carousel/owl.carousel.css');		
 		$this->document->addScript('catalog/view/javascript/jquery/owl-carousel/owl.carousel.min.js');	
 		
