c8345f5566b23f62b73bebe9067285d1b72be0af
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-20 20:26:29 +0300

    Add files via upload

--- upload/catalog/language/english/payment/paytrail.php
+++ upload/catalog/language/english/payment/paytrail.php
@@ -1,28 +1,43 @@
 <?php
-// Heading
-$_['heading_title']      = 'Paytrail';
+// Text
+$_['text_title']            = 'Paytrail';
+$_['text_gateway']          = 'Maksa verkosa';
+$_['text_paid_success']     = 'Maksu on suoritettu onnistuneesti!';
+$_['text_help_to_continue'] = 'Ole hyvä ja viimeistele tilaus lähettämällä yhteystietosi tällä sivulla olevalla lomakkeella.';
+$_['text_loading']          = 'Ladataan ...';
+$_['text_order']            = 'Tilaus nro ';
+$_['text_method_1']			= 'Nordea';
+$_['text_method_2']			= 'Osuuspankki';
+$_['text_method_3']			= 'Danske Bank';
+$_['text_method_4']			= 'Tapiola';
+$_['text_method_5']			= 'Ålandsbanken';
+$_['text_method_6']			= 'Handelsbanken';
+$_['text_method_7']		    = '???';
+$_['text_method_8']			= 'Nets';
+$_['text_method_9']			= 'Paypal';
+$_['text_method_10']		= 'S-Pankki';
+$_['text_method_11']		= 'Klarna, Laskulla';
+$_['text_method_12']		= 'Klarna, Osamaksulla';
+$_['text_method_18']		= 'Jousto';
+$_['text_method_19']		= 'Collector';
+$_['text_method_30']		= 'Visa';
+$_['text_method_31']		= 'MasterCard';
+$_['text_method_34']		= 'Diners Club';
+$_['text_method_35']		= 'JCB';
+$_['text_method_50']		= 'Aktia';
+$_['text_method_51']		= 'POP Pankki';
+$_['text_method_52']		= 'Säästöpankki';
+$_['text_method_53']		= 'Visa';
+$_['text_method_54']		= 'MasterCard';
+$_['text_method_55']		= 'Diners Club';
+$_['text_method_56']		= 'American Express';
+$_['text_method_58']		= 'MobilePay';
+$_['text_method_60']		= 'Collector Bank';
+$_['text_method_61']		= 'Oma Säästöpankki';
 
-// Text 
-$_['text_edit']          = 'Muokkaa';
-$_['text_payment']       = 'Maksutavat';
-$_['text_success']       = 'Maksutavan Paytrail muokkaaminen suoritettu!';
-$_['text_enabled']       = 'Käytössä';
-      
-// Entry
-$_['entry_merchant_id']       = 'Myyjän tunniste';
-$_['entry_merchant_secret']   = 'Salausavain';
-$_['entry_order_status']      = 'Onnistuneen tilauksen tila';
-$_['entry_order_failed_status']      = 'Epäonnisuneen tilauksen tila';
-$_['entry_geo_zone']          = 'Maantiet. alue';
-$_['entry_status']            = 'Tila';
-$_['entry_sort_order']        = 'Järjestys';
-$_['entry_box_width']         = 'Valintasivun leveys';
-
-// Help
-$_['help_box_width']         = 'Kassalle upotettavan valintasivun leveys pikseleinä (kirjoitettava ainoastaan numeroina). Oletusleveys on 1109 pikseliä.';
+$_['button_back']           = 'Peruuta';
+$_['button_confirm']        = 'Vahvista tilaus';
 
 // Error
-$_['error_permission']   = 'Varoitus: Sinulla ei ole oikeuksia Paytrail maksutavan muokkaamiseen!';
-$_['error_merchant_id']  = 'Myyjän tunniste (MERCHANT) on pakollinen tieto!';
-$_['error_secret']       = 'Salausavain on pakollinen tieto!';
+$_['error_uncomplete']      = 'Tilauksesi on vielä kesken! Ennen kuin pääset maksamnaan, syötä vaadittavat yhteystiedot lomakkeelle, ole hyvä! (Sinulla on 10 minuuttia aikaa täyttää oheinen lomake. Jos jostakin odottamattomasta syystä yhteystietojesi lähetys viisvästyy, voit lähettää tietosi kaupan sähköpostiin liittämällä mukaan kuvakaappauksen pankkisi tilillä näkyvästä maksusta).';
 ?>
