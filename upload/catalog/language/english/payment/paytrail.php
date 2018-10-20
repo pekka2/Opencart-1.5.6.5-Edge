42a1abe86cdf2fe16be000d20e1411f859cb27ab
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-20 20:23:29 +0300

    Add files via upload

---
+++ upload/catalog/language/english/payment/paytrail.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']      = 'Paytrail';
+
+// Text 
+$_['text_edit']          = 'Muokkaa';
+$_['text_payment']       = 'Maksutavat';
+$_['text_success']       = 'Maksutavan Paytrail muokkaaminen suoritettu!';
+$_['text_enabled']       = 'Käytössä';
+      
+// Entry
+$_['entry_merchant_id']       = 'Myyjän tunniste';
+$_['entry_merchant_secret']   = 'Salausavain';
+$_['entry_order_status']      = 'Onnistuneen tilauksen tila';
+$_['entry_order_failed_status']      = 'Epäonnisuneen tilauksen tila';
+$_['entry_geo_zone']          = 'Maantiet. alue';
+$_['entry_status']            = 'Tila';
+$_['entry_sort_order']        = 'Järjestys';
+$_['entry_box_width']         = 'Valintasivun leveys';
+
+// Help
+$_['help_box_width']         = 'Kassalle upotettavan valintasivun leveys pikseleinä (kirjoitettava ainoastaan numeroina). Oletusleveys on 1109 pikseliä.';
+
+// Error
+$_['error_permission']   = 'Varoitus: Sinulla ei ole oikeuksia Paytrail maksutavan muokkaamiseen!';
+$_['error_merchant_id']  = 'Myyjän tunniste (MERCHANT) on pakollinen tieto!';
+$_['error_secret']       = 'Salausavain on pakollinen tieto!';
+?>
