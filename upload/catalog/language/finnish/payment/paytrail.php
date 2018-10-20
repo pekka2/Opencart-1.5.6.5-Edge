5fdd5b7abe65a7963e2f74950d0d582d56eab659
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-20 20:26:53 +0300

    Add files via upload

---
+++ upload/catalog/language/finnish/payment/paytrail.php
@@ -0,0 +1,43 @@
+<?php
+// Text
+$_['text_title']            = 'Paytrail';
+$_['text_gateway']          = 'Maksa verkosa';
+$_['text_paid_success']     = 'Maksu on suoritettu onnistuneesti!';
+$_['text_help_to_continue'] = 'Ole hyvä ja viimeistele tilaus lähettämällä yhteystietosi tällä sivulla olevalla lomakkeella.';
+$_['text_loading']          = 'Ladataan ...';
+$_['text_order']            = 'Tilaus nro ';
+$_['text_method_1']			= 'Nordea';
+$_['text_method_2']			= 'Osuuspankki';
+$_['text_method_3']			= 'Danske Bank';
+$_['text_method_4']			= 'Tapiola';
+$_['text_method_5']			= 'Ålandsbanken';
+$_['text_method_6']			= 'Handelsbanken';
+$_['text_method_7']		    = '???';
+$_['text_method_8']			= 'Nets';
+$_['text_method_9']			= 'Paypal';
+$_['text_method_10']		= 'S-Pankki';
+$_['text_method_11']		= 'Klarna, Laskulla';
+$_['text_method_12']		= 'Klarna, Osamaksulla';
+$_['text_method_18']		= 'Jousto';
+$_['text_method_19']		= 'Collector';
+$_['text_method_30']		= 'Visa';
+$_['text_method_31']		= 'MasterCard';
+$_['text_method_34']		= 'Diners Club';
+$_['text_method_35']		= 'JCB';
+$_['text_method_50']		= 'Aktia';
+$_['text_method_51']		= 'POP Pankki';
+$_['text_method_52']		= 'Säästöpankki';
+$_['text_method_53']		= 'Visa';
+$_['text_method_54']		= 'MasterCard';
+$_['text_method_55']		= 'Diners Club';
+$_['text_method_56']		= 'American Express';
+$_['text_method_58']		= 'MobilePay';
+$_['text_method_60']		= 'Collector Bank';
+$_['text_method_61']		= 'Oma Säästöpankki';
+
+$_['button_back']           = 'Peruuta';
+$_['button_confirm']        = 'Vahvista tilaus';
+
+// Error
+$_['error_uncomplete']      = 'Tilauksesi on vielä kesken! Ennen kuin pääset maksamnaan, syötä vaadittavat yhteystiedot lomakkeelle, ole hyvä! (Sinulla on 10 minuuttia aikaa täyttää oheinen lomake. Jos jostakin odottamattomasta syystä yhteystietojesi lähetys viisvästyy, voit lähettää tietosi kaupan sähköpostiin liittämällä mukaan kuvakaappauksen pankkisi tilillä näkyvästä maksusta).';
+?>
