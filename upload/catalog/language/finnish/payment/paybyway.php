2cc49c35c0ee838c982366b59b318c64b392d32e
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-26 22:54:21 +0300

    Add files via upload

---
+++ upload/catalog/language/finnish/payment/paybyway.php
@@ -0,0 +1,38 @@
+<?php
+// Text
+$_['text_title']       = 'Maksukaista (Paybyway)';
+
+$_['text_card_payment'] = 'Korttimaksu';
+$_['text_success']     = 'Maksu suoritettiin onnistuneesti.';
+$_['text_succeeded']   = 'Masu onnistui';
+$_['text_failed']      = 'Maksu epäonnistui!';
+$_['text_is_failed']   = 'Maksu epäonnistui';
+$_['text_continue']    = 'Jatka';
+$_['text_unknow_failed'] = 'Maksu epäonnistui tuntemattomasta syystä.';
+$_['text_pay_button']  = 'Maksupainikkeet';
+$_['text_failed_1']    = 'Maksu epäonnistui. Asiakas ei onnistunut saamaan maksua loppuun.';
+$_['text_failed_4']    = 'Tapahtumien tilaa ei voitu päivittää sen jälkeen, kun asiakas on palannut maksajan avustajan verkkosivulta. Käytä kauppiaan käyttöliittymää maksun tilan korjaamiseen.';
+$_['text_failed_10']   = 'Huoltotauko. Kauppa ei ole luotu ja käyttäjä on ilmoitettu ja siirretty takaisin palautusosoitteeseen.';
+$_['text_checking']    = 'Maksun tilan tarkistaminen...';
+$_['text_token']       = 'Haetaan tunnusta...';
+$_['text_charging']    = 'Kortin lataaminen ...';
+
+// Entry
+$_['entry_card_number'] = 'Kortinnumero';
+$_['entry_month']       = 'Kuukausi';
+$_['entry_year']        = 'Vuosi';
+$_['entry_cvv']         = 'CVV';
+
+// Button
+$_['button_pay']        = 'Suorita maksu';
+
+// Help
+$_['help_cvv']          = 'Jätä CVV';
+$_['help_card_number']  = 'Jätä kortinnumero';
+
+// Error
+$_['error_payment']        = 'Korttimaksua ei voitu luoda.';
+$_['error_validation']     = 'Tietojen vahvistusvirhe:';
+$_['error_keys']           = 'Tarkista, että api-avain ja yksityinen avain ovat oikeita.';
+$_['error_create_payment'] = 'Korttimaksua ei voitu luoda. Tarkista, että api-avain ja yksityinen avain ovat oikein.';
+$_['error_exception']      = 'Havaittiin seuraava poikkeus:';
