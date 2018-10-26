ed9facd51a661c3650d93bacd4863012983f295d
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-26 22:53:54 +0300

    Add files via upload

---
+++ upload/catalog/language/english/payment/paybyway.php
@@ -0,0 +1,38 @@
+<?php
+// Text
+$_['text_title']       = 'Paybyway';
+
+$_['text_card_payment'] = 'Card Payment';
+$_['text_success']     = 'Payment was completed succesfully';
+$_['text_continue']    = 'Continue';
+$_['text_succeeded']   = 'Payment succeeded';
+$_['text_failed']      = 'Payment failed!';
+$_['text_unknow_failed'] = 'Payment failed for an unknown reason.';
+$_['text_is_failed']   = 'Payment failed';
+$_['text_pay_button']  = 'Pay by button';
+$_['text_failed_1']    = 'Payment failed. A customer did not successfully finish the payment.';
+$_['text_failed_4']    = 'Transaction status could not be updated after customer returned from a payment facilitator\'s web page. Please use the merchant UI to resolve the payment status.';
+$_['text_failed_10']   = 'Maintenance break. The transaction is not created and the user has been notified and transferred back to the return address.';
+$_['text_checking']    = 'Checking the payment status...';
+$_['text_token']       = 'Getting token...';
+$_['text_charging']    = 'Charging the card...';
+
+// Entry
+$_['entry_card_number'] = 'Card number';
+$_['entry_month']       = 'Month';
+$_['entry_year']        = 'Year';
+$_['entry_cvv']         = 'CVV';
+
+// Button
+$_['button_pay']       = 'Pay';
+
+// Help
+$_['help_card_number']  = 'Enter the Card number';
+$_['help_cvv']          = 'Enter the CVV';
+
+// Error
+$_['error_payment']        = 'Unable to create a payment.';
+$_['error_validation']     = 'Validation errors:';
+$_['error_keys']           = 'Please check that api key and private key are correct.';
+$_['error_create_payment'] = 'Unable to create card payment. Please check that api key and private key are correct.';
+$_['error_exception']      = 'Got the following exception:';
