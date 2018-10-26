3809d57bd0f8d1933f130b0605fdd7099d1b511e
Pekka Mansikka <pekka2@users.noreply.github.com>
2018-10-26 22:52:26 +0300

    Add files via upload

---
+++ upload/admin/language/finnish/payment/paybyway.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']        = 'Maksukaista';
+
+// Text
+$_['text_edit']           = 'Muokkaa';
+$_['text_payment']        = 'Ma´ksutavat';
+$_['text_clear']          = 'Tyhjennä Paybyway loki';
+$_['text_clear_success']  = 'Lokitietojen tyhjennys onnistui!';
+
+//entry
+$_['entry_api_key']        = 'API tunnisteavain';
+$_['entry_private_key']    = 'Yksityinen salausavain';
+$_['entry_order_status']   = 'Tilauksen tila';
+$_['entry_failed_status']  = 'Epäonnistuneen maksun tila';
+$_['entry_geo_zone']       = 'Maantiet. alue';
+$_['entry_status']         = 'Tila';
+$_['entry_sort_order']     = 'Järjestys:';
+$_['entry_log']            = 'Loki';
+
+// Error
+$_['error_permission']      = 'Varoitus: Sinulla ei ole oikeuksia Maksukaista maksutavan muokkaamiseen!';
+$_['error_api_key']         = 'API tunniste on pakollinen tieto!';
+$_['error_private_key']     = 'Salausavain on pakollinen tieto!';
+?>
