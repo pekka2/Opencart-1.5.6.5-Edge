6a031597bac6ad71e94944e45a916966eb9586be
Pekka Mansikka <pekka2@users.noreply.github.com>
2019-06-06 09:55:51 +0300

    Update encryption.php

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -5,7 +5,7 @@ final class Encryption {
 	
 	public function __construct($key) {
 		$this->key = hash('sha256', $key, true);
-	     if(function_exists('random_bytes)){
+	     if(function_exists('random_bytes')){
 		$this->iv = random_bytes(32);
 	     } else {
 	       $this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
