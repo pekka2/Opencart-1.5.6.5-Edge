7c6e383aefaa9c50c2f8fa3f6b6f5c36d8cb6563
Pekka Mansikka <pekka2@users.noreply.github.com>
2020-02-17 16:56:35 +0200

    Update order.php

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -233,7 +233,7 @@ class ControllerSaleOrder extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
@@ -273,7 +273,7 @@ class ControllerSaleOrder extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$url .= '&page=' . $this->request->get['page'];
+			$url .= '&page=' . (int)$this->request->get['page'];
 		}
 
 		$this->data['breadcrumbs'] = array();
